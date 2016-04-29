8b9aa388ac6dad9c248f3622b84704863f8d60ed
Raymond <rcisneros138@gmail.com>
2016-04-29 16:27:54 -0500

    first commit

---
+++ js/page.js
@@ -0,0 +1,40 @@
+//$(document).ready(function(){
+// $('.texts').textillate({
+//     autoStart:true,
+//     loop: true,
+//       in: {
+//           effect: 'flipInX',
+//           shuffle: false,
+//           sync: true,
+//           
+//       },
+//       out: {
+//             effect: 'flipOutX',
+//             shuffle: false,
+//             sync: true,
+//             
+//       },
+//        
+//    });
+//});
+
+$(function(){
+    $('.texts').textillate({
+     autoStart:true,
+     loop: true,
+       in: {
+           effect: 'flipInX',
+           shuffle: false,
+           sync: true,
+           
+       },
+       out: {
+             effect: 'flipOutX',
+             shuffle: false,
+             sync: true,
+             
+       },
+        
+    });
+});
+
